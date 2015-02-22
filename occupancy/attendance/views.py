cc8e4f69f66f9fccf0cc7435aa0c658e899857fd
Digvijay Singh <digvijay09020@iiitd.ac.in>
2015-02-22 10:29:56 +0530

    Added using exception API

--- occupancy/attendance/views.py
+++ occupancy/attendance/views.py
@@ -32,14 +32,18 @@ def curl_request_addr(address,url):
   return api_data
 
 def curl_request(url):
-  return curl_request_addr("https://192.168.1.40:9199",url)
+  return curl_request_addr("https://127.0.0.1:9119",url)
 
+def date_string():
+  return "&from=2015-02-01&to=2015-02-28&format=yyyy-mm-dd"
 
 def index(request):
   dates = []
+  p_exceptions = []
+  n_exceptions = []
   ta_info_json = {}
   if request.user and request.user.is_authenticated() :
-    api_data_url = "/attendance/get?email="+ str(request.user.email) + "&from=2015-02-01&to=2015-02-28&format=yyyy-mm-dd"
+    api_data_url = "/attendance/get?email="+ str(request.user.email) + date_string()
     api_data = curl_request(api_data_url)
     stmt = "/ta/get?email=" + str(request.user.email)
     jdata = json.loads(api_data)
@@ -48,6 +52,13 @@ def index(request):
     for date_iterator in jdata["present_dates"]:
       dates.append(date_iterator)
     print dates
+    api_url = "/exceptions/get?" + date_string()
+    api_data = curl_request(api_url)
+    exceptions_j = json.loads(api_data)
+    for date_iterator in exceptions_j["positive exceptions"]:
+      p_exceptions.append(date_iterator)
+    for date_iterator in exceptions_j["negative exceptions"]:
+      n_exceptions.append(date_iterator)
   template = loader.get_template('attendance/index.html');
-  context = RequestContext(request,{'request':request, 'user': request.user, 'dates':dates,'info':ta_info_json})
+  context = RequestContext(request,{'request':request, 'user': request.user, 'dates':dates,'info':ta_info_json,'positive_exceptions':p_exceptions,'negative_exceptions':n_exceptions})
   return HttpResponse(template.render(context))
